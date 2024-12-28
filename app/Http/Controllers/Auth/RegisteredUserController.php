0e58989f34fb49c4bdadea2c25dba21fc76f2b99
devmuhammedsalama <devmuhammedsalama@gmail.com>
2024-12-28 20:36:54 +0200

    Calling Notification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use App\Models\User;
+use App\Notifications\NewUserRegisteredNotification;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -43,6 +46,19 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
+
+        /**
+         * Send Notifications To Admin
+         */
+
+        // $admin = Admin::find(1);
+        // $admin->notify(new NewUserRegisteredNotification($user));
+        // Notification::send($admin, new NewUserRegisteredNotification($user));
+
+        Notification::send(Admin::all(), new NewUserRegisteredNotification($user));
+//        foreach ($admins as $admin) {
+//            $admin->notify(new NewUserRegisteredNotification($user));
+//        }
 
         Auth::login($user);
 
